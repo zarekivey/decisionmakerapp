9ac07017fc3dd7383aac38ce18294d3f0a282664
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-12 13:28:38 -0500

    prepping app for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,46 @@
-const path = require('path'); 
+const path = require('path'); // Node method to get the path
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = { 
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.scss' }); 
+
+    return { 
     entry: './src/app.js', 
     output: {
-        path: path.join(__dirname, 'public'), 
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'public', 'dist'), 
     },
     module: {
         rules: [{
-            loader: 'babel-loader',
+            loader:'babel-loader', 
             test: /\.js$/, 
             exclude: /node_modules/ 
         }, {
-            test: /\.s?css$/,
+            test: /\.s?css$/, 
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
             ]
         }] 
     },
-    devtool: 'cheap-module-source-map', 
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map', 
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'), 
+        publicPath: '/dist/'
     }
+};
 };
